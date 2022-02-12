bfd75c23babde6204eb0a929f9cc2de609617e2a
thetyper101 <ewcgong@hotmail.com>
2022-02-11 23:43:57 -0500

    created unit test project

---
+++ DLL_test/DLL_test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DLLtest
+{
+	TEST_CLASS(DLLtest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
