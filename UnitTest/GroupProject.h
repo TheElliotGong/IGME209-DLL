374e7e85ae711ea2f7709f86f11865e1612613d1
thetyper101 <ewcgong@hotmail.com>
2022-02-15 18:02:50 -0500

    Created functions test in unit test

---
+++ UnitTest/GroupProject.h
@@ -0,0 +1,39 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the GROUPPROJECT_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// GROUPPROJECT_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef GROUPPROJECT_EXPORTS
+#define GROUPPROJECT_API __declspec(dllexport)
+#else
+#define GROUPPROJECT_API __declspec(dllimport)
+#endif
+
+// This class is exported from the dll
+class GROUPPROJECT_API CGroupProject {
+public:
+	CGroupProject(void);
+	// TODO: add your methods here.
+};
+
+extern GROUPPROJECT_API int nGroupProject;
+
+GROUPPROJECT_API int fnGroupProject(void);
+
+//Declare GetTeam function.
+GROUPPROJECT_API char* GetTeam();
+
+GROUPPROJECT_API void SetMaze(const int** data, int width, int height);
+
+GROUPPROJECT_API int** GetMaze(int& width, int& height);
+
+GROUPPROJECT_API void GetNextPosition(int& xpos, int& ypos);
+
+GROUPPROJECT_API void SetStart(int xpos, int ypos);
+
+GROUPPROJECT_API void GetStart(int& xpos, int& ypos);
+
+GROUPPROJECT_API void SetEnd(int xpos, int ypos);
+
+GROUPPROJECT_API void GetEnd(int& xpos, int& ypos);
--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-
+#include "GroupProject.h"
+#include <cstring>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
@@ -11,6 +12,20 @@ namespace UnitTest
 		
 		TEST_METHOD(TestMethod1)
 		{
+			int x = 0;
+			int y = 0;
+			 
+			GetNextPosition(x, y);
+
+			Assert::AreEqual(4, x);
+			Assert::AreEqual(1, y);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			char* test = GetTeam();
+			char* ret = "Elliot Gong & Michael Xie";
+			Assert::AreEqual(strcmp(test,ret), 0);
 		}
 	};
 }
